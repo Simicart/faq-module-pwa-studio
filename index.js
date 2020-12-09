cad2c3e0c973d271412499a88a018f04c7a4be31
sondn272 <63297485+sondn272@users.noreply.github.com>
2020-12-09 15:59:34 +0700

    update faq homepage

---
+++ index.js
@@ -0,0 +1,72 @@
+import React, { Fragment,useState } from 'react';
+import SearchBox from '../SearchBox/searchbox'
+import CategoriesList from '../FaqsListContainer/CategoriesList'
+import Loader from '../Loader/Loader'
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
+import { Helmet } from 'react-helmet';
+import { useHomePage } from '@faq/faq/src/talons/useHomePage'
+import { useCategoryList } from '@faq/faq/src/talons/useCategoryList'
+
+const FAQ = props => {
+	const { 
+		homepageData, 
+		homepageLoading, 
+		derivedErrorMessage
+	} = useHomePage()
+	const {
+		title,
+		category_column
+	} = homepageData.MpMageplazaFaqsGetConfig.faq_home_page
+	
+	const { 
+		categoriesData,
+		categoriesLoading,
+		categoriesError 
+	} = useCategoryList()
+	const categories = categoriesData.MpMageplazaFaqsCategoryList.items;
+
+	if(!homepageData || !homepageData.MpMageplazaFaqsGetConfig) {
+		return <Loader />;
+	}
+	if(!categoriesData || !categoriesData.MpMageplazaFaqsCategoryList) {
+		return <Loader />;
+	}
+
+	const [userInput, setUserInput] = useState('');
+	const [searchInput, setSearchInput] = useState('');	
+	const [loading, setLoading] = useState(false)
+
+	const onChange = (e) => {
+		setUserInput(e.target.value)
+	}
+
+	const onSearch = () => {
+		setLoading(true)
+		setTimeout(() => {
+			setLoading(false)
+			setSearchInput(userInput)
+		}, 500)
+	}
+
+	return (
+		<Fragment>
+			<Helmet>
+                <meta charSet="utf-8" />
+                <title>{title}</title>
+            </Helmet>
+			<SearchBox
+				onChange={onChange}
+				userInput={userInput}
+				onSearch={onSearch}
+			/>
+			<CategoriesList 
+				loading={loading}
+				column={category_column} 
+				categories={categories}
+				searchInput={searchInput}
+			/>
+		</Fragment>
+	)
+}
+
+export default FAQ;
